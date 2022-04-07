899e49bd3ad9b10dabe8617b5a57e2fc795f517e
Jaemin Jo <jmjo@skku.edu>
2022-04-07 13:51:22 +0900

    Add files via upload

---
+++ components/datatable.js
@@ -0,0 +1,13 @@
+class DataTable {
+    constructor(id) {
+        this.id = id;
+    }
+
+    update(data, columns) {
+        let table = d3.select(this.id);
+
+        // TODO: create as many <tr>s as rows
+        // TODO: populate <td>s in each row        
+    }
+}
+
